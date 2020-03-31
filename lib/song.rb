e38be15b28e48e20068256f0877e5923e255067c
Justin Winger <justinwinger4@gmail.com>
2020-03-31 14:28:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,14 @@
 class Song 
   attr_accessor :name, :artist, :genre 
+  
   @@count = 0 
+  
   def self.count
-    @@count 
+    @@count += 1
+  end
+  
+  def self.count
+    @@count
   end
   
 end
