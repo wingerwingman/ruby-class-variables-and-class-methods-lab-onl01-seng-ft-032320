70a918278624c28c8a66b566756870cbf07826d0
Justin Winger <justinwinger4@gmail.com>
2020-03-31 14:47:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,19 +3,30 @@ class Song
   
   @@count = 0 
   @@artist = []
+  @@genre = []
   
   def initialize(name, artist, genre)
     @name = name
     @album = {}
     @artist = artist
     @album[artist] = []
+    @@artist << artist
     @genre = genre
     @album[genre] = []
     @@count += 1
+    @@genre << genre
   end
   
   def self.count
     @@count
+  end
+  
+  def self.count
+    @@artist
+  end
+  
+  def self.count
+    @@genre
   end
   
 end
