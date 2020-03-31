21de7afb4a5b24b42e52abfb3bb272deb9e75c04
Justin Winger <justinwinger4@gmail.com>
2020-03-31 14:34:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,22 @@
 class Song 
-  attr_accessor :name, :artist, :genre 
+  attr_reader :name, :artist, :genre 
   
   @@count = 0 
+  
+  def initialize(name)
+    @name = name
+    @album = {}
+  end
+  
+  def initialize(artist)
+    @artist = artist
+    @album[artist] = []
+  end
+  
+  def initialize(genre)
+    @genre = genre
+    @album[genre] = []
+  end
   
   def self.count
     @@count += 1
