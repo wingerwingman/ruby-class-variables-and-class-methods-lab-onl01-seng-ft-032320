2e84967824c09cf0ca5a6ea56f8ef6b4f43ef83d
Justin Winger <justinwinger4@gmail.com>
2020-03-31 16:13:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,6 +35,7 @@ class Song
         genre_count{genre = 1 
       end
     end
+    genre_count
   end
     
     def self.artist_count
@@ -46,6 +47,7 @@ class Song
           artist_count[aritst = 1 
         end
       end
+      artist_count
     end
 
   
