d7a45f99c224e8b9404bf460d86c91a9437325de
Justin Winger <justinwinger4@gmail.com>
2020-03-31 16:13:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
       if genre_count[genre]
         genre_count[genre] += 1 
       else
-        genre_count{genre = 1 
+        genre_count[genre] = 1 
       end
     end
     genre_count
